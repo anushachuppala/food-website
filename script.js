f9ed3d1bcaf18a1f9e794b61092336c4d05cf7c4
anushachuppala <anusha.chuppala12@gmail.com>
2026-03-17 21:04:30 +0530

    added js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// const x = document.querySelector()
+const navlist = document.getElementById("navlist");
+const hamburger = document.getElementById("hamburger");
+
+//click event
+
+hamburger.addEventListener("click", () => {
+  navlist.classList.toggle("navlist-active");
+});
